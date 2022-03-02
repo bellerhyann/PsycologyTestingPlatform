5142abb791996d350d019ef219d63888bf92bf6e
CJ Hess <cjh147@lindenwood.edu>
2022-03-02 12:32:08 -0600

    test 4

--- experiment.php
+++ experiment.php
@@ -1,14 +1,7 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <link rel="stylesheet" href="./src/styles.css">
-  </head>
-  <body id="user_dashboard_body">
-      <?php
-      require '/vendor/autoload.php';
-        $userID = $_GET['userID'];
-        echo "<h1> Welcome,  User #" . $userID . "!</h1>";
-        header("Location: /userDashboard.html");
-      ?>
-  </body>
-</html>
+
+<?php
+  require '/vendor/autoload.php';
+  $userID = $_GET['userID'];
+  //echo "<h1> Welcome,  User #" . $userID . "!</h1>";
+  header("Location: /userDashboard.html");
+?>
