0d1ecb2a4e178a6009de1454778ebd63413ba9ec
CJ Hess <cjh147@lindenwood.edu>
2022-03-02 12:05:26 -0600

    Update experiment.php

--- experiment.php
+++ experiment.php
@@ -7,7 +7,7 @@
       <?php
       require '/vendor/autoload.php';
         $userID = $_GET['userID'];
-        echo "<h1> Welcome,  User #" . $userID . "!</h1>";
+        // echo "<h1> Welcome,  User #" . $userID . "!</h1>";
         header("Location: /userDashboard.html");
       ?>
   </body>
