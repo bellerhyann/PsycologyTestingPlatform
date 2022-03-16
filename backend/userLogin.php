6deb446305ba68dd20d9e5a3a3b025dbe5dba157
CJ Hess <cjh147@lindenwood.edu>
2022-03-16 10:29:12 -0500

    Revision 4 - Reorganizing

--- backend/userLogin.php
+++ backend/userLogin.php
@@ -2,5 +2,5 @@
 <?php
   $userID = $_GET['userID'];
   echo "<h1> Welcome,  User #" . $userID . "!</h1>";
-  header("Location: ../frontend/userDashboard.html");
+  header("Location: ./frontend/userDashboard.html");
 ?>
