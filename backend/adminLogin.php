2638bf619305ecd5cec45cc387d66fc984bdc927
Skyeler Knuuttila <92111111+SkyeKnuuttila@users.noreply.github.com>
2022-03-21 23:13:53 -0500

    Update adminLogin.php

--- backend/adminLogin.php
+++ backend/adminLogin.php
@@ -16,11 +16,11 @@ else
         echo " connected!<br>";
 
 	//check if userID and password matches an admin
-$query = "SELECT * FROM User_T WHERE userID = $adminUser and password = \"$pass\" and isAdmin = True";
+$query = "SELECT * FROM User_T WHERE userID = $adminUser and password = \"$pass\"";
 $result = mysqli_query($conn, $query);
-if($result->num_rows == 0) {
+if(mysqli_fetch_assoc($result) == NULL) {
 	//credentials do not match
-	die("Username or Password not found." . $conn->error() );
+	die("Username or Password not found." . mysqli_connect_error());
 } else {
 	//credentials do match
 	//close connection
