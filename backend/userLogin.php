719e4f5a5813ad1d3e78de24e6ef3c415f0e282e
Skyeler Knuuttila <92111111+SkyeKnuuttila@users.noreply.github.com>
2022-03-21 20:15:53 -0500

    Update userLogin.php

--- backend/userLogin.php
+++ backend/userLogin.php
@@ -7,20 +7,21 @@ $_SESSION["userID"] = $_POST["userID"];
 	//connect to database
 echo "Attempting to connect to DB server: $host ...";
 $conn = new mysqli("newoneplease.ciqqgo3etyax.us-west-1.rds.amazonaws.com:3306", "admin", "welovesecurity!", "labdata");
-if ($conn->connect_error)
+if (!$conn)
         die("UserID not found: Database Error.".mysqli_connect_error());
 else
         echo " connected!<br>";
 
 	//check if userID matches one in the database
-$result = $conn->query("SELECT * FROM user_T WHERE userID = $userID); //int doesn't need to be in quotations
-if($result->num_rows == 0) {
+$query = "SELECT * FROM user_T WHERE userID = $userID"; //int doesn't need to be in quotations
+$result = mysqli_query($conn, $query);
+if(mysqli_fetch_assoc($result) == NULL) {
 	//credentials do not match
-	die("UserID not found." . $conn->error() );
+	die("UserID not found.");
 } else {
 	//credentials do match
 	//close connection
-	$conn->close();
+	mysqli_close($conn);
 	//redirect to dashboard.html
 	header("location: /userDashboard.html");
 	exit;
