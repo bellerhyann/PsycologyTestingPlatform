6cfe1f01263c0c5ae059d6e08c02baa701251def
bernadettekorn <77213445+bernadettekorn@users.noreply.github.com>
2022-03-21 23:31:44 -0500

    Update register.php

--- backend/register.php
+++ backend/register.php
@@ -16,15 +16,15 @@
 	}
 
 //check if name is already in database
-$queryString = ("SELECT * FROM user_T WHERE FName =\"$FName\" and LName = \"$LName\");
-$result = mysqli_query($conn, $queryString);
+//$queryString = ("SELECT * FROM user_T WHERE FName =\"$FName\" and LName = \"$LName\");
+//$result = mysqli_query($conn, $queryString);
 
-if(mysqli_fetch_assoc($result) == NULL) {
+//if(mysqli_fetch_assoc($result) == NULL) {
 	//credentials do match
-	die("Name already exists." .mysqli_connect_error());
-}
-else
-{
+	//die("Name already exists." .mysqli_connect_error());
+//}
+//else
+//{
 	//credentials do not match
 	//generate random userID
 	$userID = rand(1, 10000);
