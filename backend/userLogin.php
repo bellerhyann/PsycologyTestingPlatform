0bb85a44f9c1d6aea8af1b74fb0c3c8658d26df9
Skyeler Knuuttila <92111111+SkyeKnuuttila@users.noreply.github.com>
2022-03-21 20:36:35 -0500

    Update userLogin.php

--- backend/userLogin.php
+++ backend/userLogin.php
@@ -7,7 +7,7 @@ $_SESSION["userID"] = $_POST["userID"];
 	//connect to database
 $conn = new mysqli("newoneplease.ciqqgo3etyax.us-west-1.rds.amazonaws.com:3306", "admin", "welovesecurity!", "labdata");
 if ($conn)
-        echo " connected!<br>";
+        echo " connected.<br>";
 else
 	die("UserID not found: Database Error.".mysqli_connect_error());
 
