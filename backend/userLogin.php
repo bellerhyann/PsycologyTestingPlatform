d92cfb011dbcef121e4bb55c7598136738141924
Skyeler Knuuttila <92111111+SkyeKnuuttila@users.noreply.github.com>
2022-03-21 20:01:24 -0500

    Update userLogin.php

--- backend/userLogin.php
+++ backend/userLogin.php
@@ -1,6 +1,29 @@
-
 <?php
-  $userID = $_GET['userID'];
-  echo "<h1> Welcome,  User #" . $userID . "!</h1>";
-  header("Location: /userDashboard.html");
+//get the session log in
+session_start();
+$_SESSION["userID"] = $_POST["userID"];
+
+//reach out to database to see if the userID exists
+	//connect to database
+echo "Attempting to connect to DB server: $host ...";
+$conn = new mysqli("newoneplease.ciqqgo3etyax.us-west-1.rds.amazonaws.com:3306", "admin", "welovesecurity!", "labdata");
+if ($conn->connect_error)
+        die("UserID not found: Database Error.".mysqli_connect_error());
+else
+        echo " connected!<br>";
+
+	//check if userID matches one in the database
+$result = $conn->query("SELECT * FROM user_T WHERE userID = $userID); //int doesn't need to be in quotations
+if($result->num_rows == 0) {
+	//credentials do not match
+	die("UserID not found." . $conn->error() );
+} else {
+	//credentials do match
+	//close connection
+	$conn->close();
+	//redirect to dashboard.html
+	header("location: /userDashboard.html");
+	exit;
+}
+
 ?>
