29f5367c48f0a3db13923f66d99ca749f549f96c
bernadettekorn <77213445+bernadettekorn@users.noreply.github.com>
2022-03-23 15:34:20 -0500

    Update addPhaseInsert.php

--- backend/addPhaseInsert.php
+++ backend/addPhaseInsert.php
@@ -2,7 +2,7 @@
 //Insert new phase into phase table and blockphase table
 
 //open connection to database
-$conn = new mysqli("newoneplease.ciqqgo3etyax.us-west-1.rds.amazonaws.com:3306", "admin", "welovesecurity!", "labdata");
+$conn = new mysqli("newoneplease.ciqqgo3etyax.us-west-1.rds.amazonaws.com:3306", "admin", "Ilovesecurity!", "labdata");
 
 $phaseID = $_POST["phaseID"];
 $allowedTime = $_POST["allowedTime"];
