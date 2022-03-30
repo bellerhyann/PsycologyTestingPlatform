c3863fea1b6b462f37328b9e2243e59ee18b67ff
CJ Hess <cjh147@lindenwood.edu>
2022-03-30 11:10:28 -0500

    Update uploadStimuli.php

--- backend/uploadStimuli.php
+++ backend/uploadStimuli.php
@@ -20,8 +20,8 @@
   ]);
 
   $filename = $_FILES['imgFile']['name'];
-  $bucket = 'behaviorsci-assets';
-  $file_Path = '/stimuli/images/'.DIRECTORY_SEPARATOR.$filename;
+  $bucket = 'behaviorsci-assets/stimuli/images';
+  $file_Path = $_SERVER['DOCUMENT_ROOT'].'/upload/'.$filename;
   $source = fopen($file_Path, 'rb');
   $key = basename($file_Path);
 
