d4ed3825065a74d097395df0ed46af0c39a72ff2
bernadettekorn <77213445+bernadettekorn@users.noreply.github.com>
2022-04-02 12:14:50 -0500

    Update register.php

--- backend/register.php
+++ backend/register.php
@@ -1,17 +1,17 @@
 <?php
-	//get the session log in
-	$FName = $_POST["FName"];
-	$LName = $_POST["LName"];
-	//reach out to database to add the name and generate a userID
-	//connect to database
-	echo "Attempting to connect to DB server: ...";
-	$conn = new mysqli("newoneplease.ciqqgo3etyax.us-west-1.rds.amazonaws.com:3306","admin","Ilovesecurity!","labdata",3306);
-	if (!$conn) {
-		die("Unable to Connect.".mysqli_connect_error());
-	}
-	else {
-		echo " connected!";
-	}
+//get the session log in
+$FName = $_POST["FName"];
+$LName = $_POST["LName"];
+//reach out to database to add the name and generate a userID
+//connect to database
+echo "Attempting to connect to DB server: ...";
+$conn = new mysqli("newoneplease.ciqqgo3etyax.us-west-1.rds.amazonaws.com:3306","admin","Ilovesecurity!","labdata",3306);
+if (!$conn) {
+	die("Unable to Connect.".mysqli_connect_error());
+}
+else {
+	echo " connected!";
+}
 //check if name is already in database
 $queryString = ("SELECT * FROM user_T WHERE FName =\"$FName\" and LName = \"$LName\"");
 $result = mysqli_query($conn, $queryString);
@@ -24,32 +24,22 @@ if(mysqli_num_rows($result) > 0){
 //generate random userID
 $userID = rand(1, 999) + 1000;
 
-	//make sure userID does not exist
-	//$result = $conn->query("SELECT * FROM user_T WHERE userID = $userID");
-	//while($result->num_rows != 0) { //I am not actually sure if this is going to work like this
+//make sure userID does not exist
+$result = $conn->query("SELECT * FROM user_T WHERE userID = $userID");
+while($result->num_rows != 0) { //I am not actually sure if this is going to work like this
 		//userID does already exist
-		//generate new random userID
-		//$userID = rand(1, 10000);
-		//$result = $conn->query("SELECT * FROM user_T WHERE userID = $userID");
-	//}	
-	//add new user to database
-	//$queryString = ("INSERT INTO user_T values ($userID, \"$FName\", \"$LName\", 1, NULL)");
-	//mysqli_query($conn, $queryString);
+//generate new random userID
+$userID = rand(1, 999) + 1000;
+$result = $conn->query("SELECT * FROM user_T WHERE userID = $userID");
+	}	
 
+//add new user to database
+$queryString = ("INSERT INTO user_T values ($userID, \"$FName\", \"$LName\", 1, NULL)");
+mysqli_query($conn, $queryString);
 
-    //add new user to database
-    $queryString = ("INSERT INTO user_T values ($userID, \"$FName\", \"$LName\", 1, NULL)");
-	mysqli_query($conn, $queryString);
+//close connection
+mysqli_close($conn);
 
-	//close connection
-	mysqli_close($conn);
-// 	//close connection
-// 	mysqli_close($conn);
-
-	echo "Completed. Please contact admin for further instructions.";
-
-
-// } 
-
+echo "Completed. Please contact admin for further instructions.";
 
 ?>
