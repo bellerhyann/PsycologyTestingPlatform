12c40748cd3f1b4584e609dca13141179c4667f0
CJ Hess <cjh147@lindenwood.edu>
2022-04-06 12:51:13 -0500

    Update deleteStimuli.php

--- backend/deleteStimuli.php
+++ backend/deleteStimuli.php
@@ -1,3 +1,36 @@
 <?php
   // File that deletes a stimuli from S3 and the SQL DB
+  define('HOST', 'https://s3.us-west-1.amazonaws.com');
+  define('REGION', 'us-west-1');
+
+  require 'vendor/autoload.php';
+  
+  use Aws\S3\S3Client;
+  use Aws\S3\ObjectUploader;
+
+  // Instantiate an Amazon S3 client.
+  $s3Client = new S3Client([
+      'version' => 'latest',
+      'region'  => REGION,
+      'endpoint' => HOST,
+      'credentials' => [
+          'key'    => $_SERVER["AWS_KEY"],
+          'secret' => $_SERVER["AWS_SECRET_KEY"]
+      ]
+  ]);
+
+  $bucket = 'behaviorsci-assets';
+
+  try {
+    $result = $s3Client->listObject([
+      'Bucket' => $bucket
+    ]);
+
+    foreach ($objects['Contents']  as $object) {
+      echo $object['Key'] . PHP_EOL;
+  }
+  }
+  catch (Aws\S3\Exception\S3Exception $e){
+    echo $e->getMessage() . PHP_EOL;
+  }
 ?>
