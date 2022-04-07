f63b9ef4aba96bd164633b0220478a2f7df365bf
bernadettekorn <77213445+bernadettekorn@users.noreply.github.com>
2022-04-07 11:15:43 -0500

    Update viewuser.php

--- backend/viewuser.php
+++ backend/viewuser.php
@@ -9,11 +9,11 @@ $queryString = "SELECT * FROM user_T";
 $result = mysqli_query($conn, $queryString);
 
 echo "<table border=1>";
-echo "<tr> <th>First Name</th> <th>User ID</th> </tr>";
-while ($row = mysqli_fetch_array($result))
+echo "<tr> <th>First Name</th> <th>Last Name </th><th>User ID</th> </tr>";
+while ($row = sqli_fetch_assoc($result))
   {
     //printf("Select returned %d rows.\n", $result->userID)
-	echo "<tr> <td>".$row["FName"]."</td>".
+	echo "<tr> <td>".$row["FName"]."</td>"."<td>".$row["LName"].
   	"<td>".$row["userID"]."</td> </tr>";
   }
 
