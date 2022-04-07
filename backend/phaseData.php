76599f470ed90bc43a99c5bac7f04d98c4490689
bernadettekorn <77213445+bernadettekorn@users.noreply.github.com>
2022-04-07 13:46:21 -0500

    Update phaseData.php

--- backend/phaseData.php
+++ backend/phaseData.php
@@ -65,10 +65,12 @@ while ($row = mysqli_fetch_array($block))
         }
 
         $count = mysqli_query($conn, $queryString);
+	$top = $count;
+	$bot = $userNum;
 
         //this gives us a decimal with .00 and then multiple by 100 to give us the % - DOES NOT WORK BEACUSE NOT STRING
         //$correctPER = (bcdiv($count,$userNum, 2)) * 100;
-	$correctPER = $count/$userNum;
+	$correctPER = $top/$bot;
     }
 
 }
