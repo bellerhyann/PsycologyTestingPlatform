8b8b787ede564d141bac0858fd3efafa0a0d22c6
CJ Hess <cjh147@lindenwood.edu>
2022-04-11 11:36:34 -0500

    Edit Stim
    edit should be finished full upload to S3, needs SQL involved.

--- backend/uploadStimuli.php
+++ backend/uploadStimuli.php
@@ -20,52 +20,24 @@
       ]
   ]);
 
-  $soundFile = $_POST['stim_key'] . '.wav';
-  $imgFile = $_POST['stim_key'] . '.png';
 
-  $filename = $_POST['stim_key'].substr($_FILES['imgFile']['name'],-4);
-  echo $filename;
+  $fileName = $_POST['stim_key']; // Name of uploaded file
+  $fileExtension = $_POST['uFile']['type']; // Type of uploaded file
+
   $bucket = 'behaviorsci-assets';
   $destination_path = getcwd().DIRECTORY_SEPARATOR;
-  
-  // First check if a file exists with the stim_key same, but not same extension
-  $soundFileExists = $s3Client->doesObjectExist($bucket, $soundFile);
-  $imgFileExists = $s3Client->doesObjectExist($bucket, $imgFile);
 
-  // If we catch that case - delete the current file
-  try {
-    if ($soundFileExists and $soundFile != $filename) {
-      // delete sound file
-      $deleteStimuli = $s3Client->deleteObject([
-        'Bucket' => $bucket,
-        'Key' => $soundFile
-      ]);
-      if ($deleteStimuli['DeleteMarker']) {
-        echo '<p> Successfully deleted ' . $soundFile . ' </p>' . PHP_EOL;
-      }
-    }
-    else if ($imgFileExists and $imgFile != $filename) {
-      // delete image file
-      $deleteStimuli = $s3Client->deleteObject([
-        'Bucket' => $bucket,
-        'Key' => $imgFile
-      ]);
-      if ($deleteStimuli['DeleteMarker']) {
-        echo '<p> Successfully deleted ' . $imgFile . ' </p>' . PHP_EOL;
-      }
-    }
-  }
-  catch (Aws\S3\Exception\S3Exception $e) {
-    echo $e->getMessage();
-  }
+  echo $fileName."<br>";
+  echo $fileExtension."<br>";
+
 
   // Then we upload to S3 normally
   // Uploading something that has the same full name - will just update on upload
 
   // Upload file to S3
-  if (move_uploaded_file($_FILES['imgFile']['tmp_name'], $destination_path.basename($filename))) {
+  if (move_uploaded_file($_FILES['uFile']['tmp_name'], $destination_path.basename($fileName))) {
     try {
-      $file_Path = $destination_path.basename($filename);
+      $file_Path = $destination_path.basename($fileName);
       $key = basename($file_Path);
       $source = fopen($file_Path, 'rb');
 
