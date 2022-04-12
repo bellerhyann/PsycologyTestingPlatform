25051e76d38a874504c3734445447d58fb0b36bc
Skyeler Knuuttila <92111111+SkyeKnuuttila@users.noreply.github.com>
2022-04-12 09:35:55 -0500

    Update displayUserBlockResults.php

--- backend/displayUserBlockResults.php
+++ backend/displayUserBlockResults.php
@@ -1,7 +1,9 @@
-<? php
+<?php
 //get blockID from post
-$blockID = $_POST["blockID"];
-$userID = $_POST["userID"];
+//$blockID = $_POST["blockID"];
+//$userID = $_POST["userID"];
+$blockID = 1;
+$userID = 1223;
 
 //open connection to database
 $conn = new mysqli("newoneplease.ciqqgo3etyax.us-west-1.rds.amazonaws.com:3306", "admin", "Ilovesecurity!", "labdata");
@@ -10,15 +12,17 @@ if (!$conn)
 
 //find how many trials are in this block
 $queryString = "SELECT trialNum FROM block_T WHERE blockID = $blockID";
-$count =  mysqli_query($conn, $queryString);
+$trialNum =  mysqli_query($conn, $queryString);
+$count = $trialNum->fetch_assoc();
+$count = $count['trialNum'];
 
-//create view where blockID is used
-$createView = "create view dataUserBlock if not exists AS SELECT * FROM data_T WHERE blockID = $blockID AND userID = $userID";
+//create view where blockID and userID are used
+$createView = "create view dataBlock AS SELECT * FROM data_T WHERE blockID = $blockID AND userID = $userID";
 //run query
 $result =  mysqli_query($conn, $createView);
 
 //create query
-$queryString = "SELECT stimIDOne, stimIDTwo, isCorrect, clicked, clickTime, phaseID FROM trial_T, dataUserBlock WHERE trialID.trial_T = trialID.dataUserBlock GROUP BY phaseID";
+$queryString = "SELECT stimIDOne, stimIDTwo, isCorrect, clicked, clickTime, phaseID FROM trial_T, dataBlock WHERE trial_T.trialID = dataBlock.trialID GROUP BY phaseID";
 //run query
 $stats =  mysqli_query($conn, $queryString);
 
@@ -26,26 +30,39 @@ $stats =  mysqli_query($conn, $queryString);
 $myfile = fopen("renameMe.txt", "w") or die("Unable to open file!");
 
 //iterate through query and add each line to the file
+$i = 0;
 while($row = mysqli_fetch_assoc($stats)) {
   //if this is the start of a block
-  if($row % $count == 0) {
+  $test = bcmod($i, $count);
+  if($test == 0) {
     //write top of file info
     $txt = "UserID:\t" . $userID . "\n BlockID:\t" . $row["blockID"] . "\n PhaseID:\t" . $row["phaseID"] . "\n";
     fwrite($myfile, $txt);
-    $txt = "Stim1\t| Stim2\t| Match\t| Comparison Time\t| User Clicked\n";
+    $txt = "Stim1\t| Stim2\t| Match\t| Comparison Time  | User Clicked\n";
     fwrite($myfile, $txt);
   }
   
-  $txt = $row["stimIDOne"] . "\t| " . $row["stimIDTwo"] . "\t| " . $row["isCorrect"] . "\t| " . $row["clickTime"] . "\t| " . $row["clicked"] . "\n";
+  $txt = $row["stimIDOne"] . "\t| " . $row["stimIDTwo"] . "\t| " . $row["isCorrect"] . "\t| " . $row["clickTime"] . "\t\t| " . $row["clicked"] . "\n";
   fwrite($myfile, $txt);
-  
+        
+  $i = $i + 1;  
 }
 
 //delete view
-$dropView = "DROP VIEW[dataUserBlock]";
+$dropView = "DROP VIEW dataBlock";
 $result =  mysqli_query($conn, $dropView);
 
 //close connection
 mysqli_close($conn);
+
+fclose($myfile);
+header('Content-Description: File Transfer');
+header('Content-Disposition: attachment; filename='.basename("renameMe.txt"));
+header('Expires: 0');
+header('Cache-Control: must-revalidate');
+header('Pragma: public');
+header('Content-Length: ' . filesize("renameMe.txt"));
+header("Content-Type: text/plain");
+readfile("renameMe.txt");
 
 ?>
