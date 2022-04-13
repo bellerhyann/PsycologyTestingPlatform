ae77e95e9f738409ebee2aeaaf24177717535e27
bernadettekorn <77213445+bernadettekorn@users.noreply.github.com>
2022-04-12 19:14:00 -0500

    Update adminLogin.php

--- backend/adminLogin.php
+++ backend/adminLogin.php
@@ -11,7 +11,7 @@ $password = $_POST["password"];
 
 //reach out to database to see if the credentials match admin credentials
 	//connect to database
-$conn = new mysqli("newoneplease.ciqqgo3etyax.us-west-1.rds.amazonaws.com:3306", "admin", "Ilovesecurity!", "labdata");
+$conn = new mysqli("us-cdbr-east-05.cleardb.net:3306", "b5541841c18a2e", "ee93a776", "heroku_8eb08016ed835ac");
 if (!$conn)
         die("Username or Password not found: Database Error.".mysqli_connect_error());
 
