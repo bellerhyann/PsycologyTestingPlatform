59168b08b17fdf937ac6102f2b95619fbf6f27a1
bernadettekorn <77213445+bernadettekorn@users.noreply.github.com>
2022-04-12 19:37:55 -0500

    Update question.php

--- question.php
+++ question.php
@@ -29,7 +29,7 @@
         {
           <?php 
             $conn = new mysqli("us-cdbr-east-05.cleardb.net:3306", "b5541841c18a2e", "ee93a776", "heroku_8eb08016ed835ac");
-            $queryString = ("SELECT stimID, stimType FROM labdata.stimuli_T"); // grab stim data from stimuli datatable
+            $queryString = ("SELECT stimID, stimType FROM stimuli_T"); // grab stim data from stimuli datatable
             $stimIDs = mysqli_query($conn, $queryString); // query with above info
 
             $i = 0; // incrementor variable
