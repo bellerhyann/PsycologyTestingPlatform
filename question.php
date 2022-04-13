c08abf10c506a15b22541ef51b47c123cb963406
bernadettekorn <77213445+bernadettekorn@users.noreply.github.com>
2022-04-12 19:34:00 -0500

    Update question.php

--- question.php
+++ question.php
@@ -28,7 +28,7 @@
         function getQuestionData()
         {
           <?php 
-            $conn = new mysqli("newoneplease.ciqqgo3etyax.us-west-1.rds.amazonaws.com:3306","admin","Ilovesecurity!","labdata",3306); // establish mysqli connection
+            $conn = new mysqli("us-cdbr-east-05.cleardb.net:3306", "b5541841c18a2e", "ee93a776", "heroku_8eb08016ed835ac");
             $queryString = ("SELECT stimID, stimType FROM labdata.stimuli_T"); // grab stim data from stimuli datatable
             $stimIDs = mysqli_query($conn, $queryString); // query with above info
 
