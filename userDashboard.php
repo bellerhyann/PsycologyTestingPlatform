984dd8f01fb8c8d81478c0c727c69f5c34b257e5
Chris Barry <chrisdavebarry@gmail.com>
2022-04-14 18:50:05 -0500

    Update userDashboard.php

--- userDashboard.php
+++ userDashboard.php
@@ -67,7 +67,7 @@
   <!-- For CSS, make new id for user dashboard-->
   <body id="user_dashboard_body">
       <div id="dashboard">
-          <h1 id="welcomeUserMsg">Welcome, User#<?php session_start(); echo $_SESSION["FName"]; ?>!</h1>
+          <h1 id="welcomeUserMsg">Welcome, User#<?php session_start(); echo $_SESSION("FName"); ?>!</h1>
           <!-- Iron out the example test -->
           <!-- For CSS, make new id for begin session button-->
           <h1><a href="./question.php" class="button" id="beginSessionButton">Begin Session</a></h1>
