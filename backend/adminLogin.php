b5850823981ac820766dae374469b439855750ab
Skyeler Knuuttila <92111111+SkyeKnuuttila@users.noreply.github.com>
2022-04-14 22:03:15 -0500

    Update adminLogin.php

--- backend/adminLogin.php
+++ backend/adminLogin.php
@@ -5,7 +5,7 @@ $password = $_POST["password"];
 
 //if we need to save the login info as a session, this code will work
 session_start();
-$_SESSION["adminUserID"] = $adminUser;
+$_SESSION["adminUserID"] = $adminUserID;
 $_SESSION["password"] = $password;
 
 
