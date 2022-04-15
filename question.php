e299e27b68e050dfb16b9a1d86bc88d20d909f8b
Chris Barry <chrisdavebarry@gmail.com>
2022-04-14 21:58:25 -0500

    Update question.php

--- question.php
+++ question.php
@@ -31,8 +31,6 @@
           <?php 
             $conn = new mysqli("us-cdbr-east-05.cleardb.net:3306", "b5541841c18a2e", "ee93a776", "heroku_8eb08016ed835ac");
             $queryString = ("SELECT stimID, stimType FROM stimuli_T"); // grab stim data from stimuli datatable
-            session_start(); 
-            echo $_SESSION["sessionID"];
             $stimIDs = mysqli_query($conn, $queryString); // query with above info
 
             $i = 0; // incrementor variable
