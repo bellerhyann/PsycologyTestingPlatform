15fa516f3c13afb50868356823670ff13ec48d9e
Skyeler Knuuttila <92111111+SkyeKnuuttila@users.noreply.github.com>
2022-04-14 21:56:30 -0500

    Update adminDashboard.php

--- adminDashboard.php
+++ adminDashboard.php
@@ -27,6 +27,7 @@
 		    				$userID = $_SESSION["adminUserID"];
 		    				$queryString = ("SELECT FName FROM user_T WHERE userID = $userID");
 		    				$result =  mysqli_query($conn, $queryString);
+		    				echo gettype($result);
 						while ($row=mysqli_fetch_row($result)) {
 							echo $row[0];	
 						}
