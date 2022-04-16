f2c7c37e6fafa64a05cab229fac909cb128ad0ce
Chris Barry <chrisdavebarry@gmail.com>
2022-04-16 16:08:33 -0500

    Update question.php

--- question.php
+++ question.php
@@ -135,7 +135,6 @@
 
     <p id="arrayData">
       <?php 
-        session_start(); 
         $conn = new mysqli("us-cdbr-east-05.cleardb.net:3306", "b5541841c18a2e", "ee93a776", "heroku_8eb08016ed835ac"); 
         if (!$conn)
             die("Database Error.".mysqli_connect_error());
