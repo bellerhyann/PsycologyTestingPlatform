fac00272a46c44bed54ba086032a91230ab7a574
Skyeler Knuuttila <92111111+SkyeKnuuttila@users.noreply.github.com>
2022-04-24 19:56:58 -0500

    Update question.php

--- question.php
+++ question.php
@@ -17,30 +17,37 @@
 
           imageStim = document.getElementById("imageStim");
           soundStim = document.getElementById("soundStim");
-          getUserData();
+          getBlockList();
           getQuestionData(); // gets all question data from database
           getNextComparison(0); // gets next comparison
         }
 
 	
-        function getUserData()
+        function getBlockList()
         { 
           console.log
           (
             <?php 
-              session_start();
-              $conn = new mysqli("us-cdbr-east-05.cleardb.net:3306", "b5541841c18a2e", "ee93a776", "heroku_8eb08016ed835ac"); 
-              if (!$conn)
-                  die("Database Error.".mysqli_connect_error());
+		//Author: Skyeler Knuuttila
+              	session_start();
+              	$conn = new mysqli("us-cdbr-east-05.cleardb.net:3306", "b5541841c18a2e", "ee93a776", "heroku_8eb08016ed835ac"); 
+              	if (!$conn)
+                  	die("Database Error.".mysqli_connect_error());
 		  
-              $userID = $_SESSION["userID"];
-              $queryString = ("SELECT phaseID FROM user_T WHERE userID = $userID");
-              $result =  mysqli_query($conn, $queryString);
-	      $userPH = $result->fetch_assoc();
-	      $userPH = $userPH['phaseID']; //userPH now stores the phase yje user is on 
-		  
-	      //CREATE ARRAY OF BLOCKIDS 
-	      //session the array   
+	      	//find current phase
+              	$userID = $_SESSION["userID"];
+              	$queryString = ("SELECT phaseID FROM user_T WHERE userID = $userID");
+              	$result =  mysqli_query($conn, $queryString);
+	      	$userPH = $result->fetch_assoc();
+	      	$userPH = $userPH['phaseID']; //userPH now stores the phase the user is on 
+	
+	      	//create an array of blockID's from that phase 
+		$blockList = array();
+	      	$queryString = ("SELECT blockID FROM phaseBlock_T WHERE phaseID = $phaseID ORDER BY __"); //have to change the name of this item in the database
+	      	$result =  mysqli_query($conn, $queryString);
+	      	while($row = mysqli_fetch_array($result)) {
+    			array_push($blockList, $row);
+		}
 	     ?>
 	  //create the json array 
           );
