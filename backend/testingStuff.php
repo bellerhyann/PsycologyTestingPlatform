8f587e493078d6bb1f6563e65606a3199dcfd217
Skyeler Knuuttila <92111111+SkyeKnuuttila@users.noreply.github.com>
2022-04-24 20:57:48 -0500

    Update testingStuff.php

--- backend/testingStuff.php
+++ backend/testingStuff.php
@@ -1,25 +1,30 @@
 <?php 
-		//Author: Skyeler Knuuttila
-    //using this file to test things for question.php
+	//Author: Skyeler Knuuttila
+	//given blockID, return array of all stim and stimTypes
+	//in form ["A1.png", "image", "B1.wav", "sound", .....]
+	/*$blockID = 1; //given from json
 
-
-    $conn = new mysqli("us-cdbr-east-05.cleardb.net:3306", "b5541841c18a2e", "ee93a776", "heroku_8eb08016ed835ac"); 
-    if (!$conn)
-    die("Database Error.".mysqli_connect_error());
-		  
-	  //find current phase
-    $userID = 1066;
-    $queryString = ("SELECT phaseID FROM user_T WHERE userID = $userID");
-    $result =  mysqli_query($conn, $queryString);
-	  $userPH = $result->fetch_assoc();
-	  $userPH = $userPH['phaseID']; //userPH now stores the phase the user is on 
+	$conn = new mysqli("us-cdbr-east-05.cleardb.net:3306", "b5541841c18a2e", "ee93a776", "heroku_8eb08016ed835ac");
+	if (!$conn)
+        	die("BlockID not found: Database Error.".mysqli_connect_error());
 	
-	  //create an array of blockID's from that phase 
-		$blockList = array();
-	  $queryString = ("SELECT blockID FROM phaseBlock_T WHERE phaseID = $userPH ORDER BY blockOrder");
-	  $result =  mysqli_query($conn, $queryString);
-	  while($row = mysqli_fetch_assoc($result)) {
-    		array_push($blockList, $row['blockID']);
-	  }
-	echo implode(" ", $blockList);
+	//start with an array of trialID's
+	$trialList = array(); //empty array
+	$queryString = ("SELECT trialID FROM blockTrial_T WHERE blockID = $blockID ORDER BY trialOrder");
+	$result =  mysqli_query($conn, $queryString);
+	while($row = mysqli_fetch_array($result)) {
+    		array_push($trialList, $row['trialID']);
+	}
+
+	//get array of stimuli by trial
+	$stimList = array();*/
+	//for ($i = 0; $i <= sizeOf($trialList); $i++) {
+		$queryString = ("SELECT * FROM trial_T, stimuli_T WHERE trialID = "A2C2" AND stimIDOne = stimID OR trialID = "A2C2" AND stimIDTwo = stimID;");
+	      	$result =  mysqli_query($conn, $queryString);
+	      	while($row = mysqli_fetch_array($result)) {
+			echo $row['stimID'];
+    			//array_push($stimList, $row['stimID']);
+			//array_push($stimList, $row['stimType']);
+		}	
+	//}
 ?>
