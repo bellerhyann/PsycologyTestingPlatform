e7f614f6d542cc639286c5966526764e6663f8d9
Skyeler Knuuttila <92111111+SkyeKnuuttila@users.noreply.github.com>
2022-04-24 20:23:33 -0500

    Update testingStuff.php

--- backend/testingStuff.php
+++ backend/testingStuff.php
@@ -19,7 +19,7 @@
 	  $queryString = ("SELECT blockID FROM phaseBlock_T WHERE phaseID = $userPH ORDER BY blockOrder");
 	  $result =  mysqli_query($conn, $queryString);
 	  while($row = mysqli_fetch_assoc($result)) {
-    		array_push($row['blockID'], $blockList);
+    		array_push($blockList, $row['blockID']);
 	  }
 	echo implode(" ", $blockList);
 ?>
