8af4e0bc8eb8def22daa31b7ed126494a8dda153
Kumpei <44628106+KEEEi@users.noreply.github.com>
2022-04-26 10:01:10 -0500

    Update userDelete.php

--- userDelete.php
+++ userDelete.php
@@ -1,4 +1,8 @@
 <?php
+
+$link_main = "adminDashboard.php";
+
+
 $conn = new mysqli("us-cdbr-east-05.cleardb.net:3306", "b5541841c18a2e", "ee93a776", "heroku_8eb08016ed835ac");
 
 if (!$conn) {
@@ -12,9 +16,11 @@ $user_id = $_POST["user_id"];
 $cmd = "DELETE FROM user_T WHERE userID = '$user_id'";
 $result1 = mysqli_query($conn, $cmd);
 if (!$result1) {
-    print("Failed to delete data<br>");
+   	print("Failed to delete data<br>");
+} else {
+	print("Success to delete data<br>");
 }
  
 mysqli_close($conn);
-
+echo "<a href=", $link_main ,">", "Go back to Main Page","</a>";
 ?>
