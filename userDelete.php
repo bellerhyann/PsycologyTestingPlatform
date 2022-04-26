c68c6af7424395928af05c9c5dff11e5f0280af8
Kumpei <44628106+KEEEi@users.noreply.github.com>
2022-04-26 09:54:51 -0500

    Update userDelete.php

--- userDelete.php
+++ userDelete.php
@@ -9,5 +9,12 @@ $user_id = $_POST["user_id"];
 
 
 //Delete specific data
+$cmd = 'DELETE FROM user_T WHERE userID = $user_id';
+$result1 = mysqli_query($conn, $cmd);
+if (!$result1) {
+    print("Failed to delete data<br>");
+}
+ 
+mysqli_close($conn);
 
 ?>
