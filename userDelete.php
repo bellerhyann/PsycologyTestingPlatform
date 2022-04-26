6a0492701250b10e45ce85d771c912a06d8fabd0
Kumpei <44628106+KEEEi@users.noreply.github.com>
2022-04-26 10:17:53 -0500

    Update userDelete.php

--- userDelete.php
+++ userDelete.php
@@ -12,7 +12,11 @@ if (!$conn) {
 $stmt = $mysqli->prepare('DELETE * FROM user_T WHERE Password IS NULL');
 
 $stmt->execute();
- 
+
+$stmt = $mysqli->prepare('DELETE * FROM data_T');
+
+$stmt->execute();
+
 mysqli_close($conn);
 echo "<a href=", $link_main ,">", "Go back to Main Page","</a>";
 ?>
