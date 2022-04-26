ee2e1a4eaebc8c1aaaf6a18e6d1f6b622792b77f
Kumpei <44628106+KEEEi@users.noreply.github.com>
2022-04-26 09:39:42 -0500

    Update userDelete.php

--- userDelete.php
+++ userDelete.php
@@ -5,10 +5,7 @@ if( $mysqli->connect_errno ) {
 	echo $mysqli->connect_errno . ' : ' . $mysqli->connect_error;
 }
 
-$first_name = $_POST["first_name"];
-$last_name = $_POST["last_name"];
 $user_id = $_POST["user_id"];
-$phase_id = $_POST["phase_id"];
 
 
 //Delete specific data
