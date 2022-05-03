879d86670f4af8e88f2d33c7757d1d1bb3e07ee7
Chris Barry <chrisdavebarry@gmail.com>
2022-05-03 18:13:58 -0500

    Update question.php

--- question.php
+++ question.php
@@ -109,7 +109,7 @@
         //console.log("Got image file: ", imageStim.src);
       }
       currIndex += 2; // advance to next stimID in block list
-
+      console.log(block.length);
       if(currIndex == block.length-1) // reached end of current block
         currBlock ++;
 
