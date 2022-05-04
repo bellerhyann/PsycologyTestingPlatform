8b9e4d2affcf081a6e29e6b9f4edc3169d708509
nwood97 <86673913+nwood97@users.noreply.github.com>
2022-05-04 18:23:37 -0500

    Update register.php

--- backend/register.php
+++ backend/register.php
@@ -8,15 +8,19 @@ $LName = $_POST["LName"];
 if(empty($FName) || empty($LName)){
 	echo "<script> alert('Please enter a first AND last name!');window.location='../register.html'</script>";
 }
+$hasNumber = false;
 for($i = 0; $i < strlen($FName); $i++){
-	if(ctype_digit($FName[$i])){
-		echo "<script> alert('Please don't enter any numbers into first name!');window.location='../register.html'</script>";
+	if((ord($FName[$i]) > 47) && (ord($FName[$i]) < 58)){
+		$hasNumber = true;
 	}
 }
 for($i = 0; $i < strlen($LName); $i++){
-	if(ctype_digit($LName[$i])){
-		echo "<script> alert('Please don't enter any numbers into last name!');window.location='../register.html'</script>";
+	if((ord($LName[$i]) > 47) && (ord($LName[$i]) < 58)){
+		$hasNumber = true;
 	}
+}
+if($hasNumber == true){
+	echo "<script> alert('Please don't enter any numbers into last name!');window.location='../register.html'</script>";
 }
 
 //reach out to database to add the name and generate a userID
