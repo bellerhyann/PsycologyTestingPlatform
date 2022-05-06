e2a7dc1101e9132625ba0943e06a42fe6c2605dc
CJ Hess <cjh147@lindenwood.edu>
2022-05-05 22:55:21 -0500

    Update uploadStimuli.php

--- backend/uploadStimuli.php
+++ backend/uploadStimuli.php
@@ -61,12 +61,11 @@
       $result = $uploader->upload();
       if ($result['@metadata']['statusCode'] == '200') {
         print('<p>File successfully uploaded to ' . $result["ObjectURL"] . '.</p><br>'); 
+        print('<a href="editStim.php">Return to Edit Stim</a>');
       }
     }
     catch (Aws\S3\Exception\S3Exception $e) {
       echo $e->getMessage();
     }
-
-    print('<a href="editStim.php">Return to Edit Stim</a>');
   }
 ?>
