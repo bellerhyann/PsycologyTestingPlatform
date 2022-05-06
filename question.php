dddbd5f4362044ceeca6cfecfd21b7321e57d4ce
Chris Barry <chrisdavebarry@gmail.com>
2022-05-05 21:45:27 -0500

    Update question.php

--- question.php
+++ question.php
@@ -226,12 +226,12 @@
 <body id="body" onload="onLoad()">
   <img id="questionHelpButton" src="../images/questionHelpButton.png" width="50" height="50">
   <div id="questionHelpPrompt">
-      i.	After the user clicks “Begin Session” on the User Dashboard Page (2d), it will take them to the question page, 
-  <br>ii.	At the beginning of each phase a “Start” button will appar on the screan, after being clicked, a Sound will automatically play through the browser, or an image will display on the screen for up to 5 seconds. 
-  <br>iii.	3 seconds after the first image/sound plays another image/sound will display/play. 
-  <br>iv.	Once the second sound/image finishes, a white box will appear, the user has up to 8 seconds to click the box if the sound/image pair matches or are similar. 
-  <br>v.	The process then repeats until the end of the phase.
-<br>Line 2 <br>Line 3 <br></div><br>
+        i.	After the user clicks “Begin Session” on the User Dashboard Page (2d), it will take them to the question page, 
+    <br>ii.	At the beginning of each phase a “Start” button will appar on the screan, after being clicked, a Sound will automatically play through the browser, or an image will display on the screen for up to 5 seconds. 
+    <br>iii.	3 seconds after the first image/sound plays another image/sound will display/play. 
+    <br>iv.	Once the second sound/image finishes, a white box will appear, the user has up to 8 seconds to click the box if the sound/image pair matches or are similar. 
+    <br>v.	The process then repeats until the end of the phase.
+  </div><br>
 
   <a id="nextQuestionButton" onclick="getNextQuestion()"></a>
   <div id="boxMain">
